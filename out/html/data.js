a12adf5649e7307e290cf5ed79294835ecf40854
Commie45 <rithik.karadiyaan@icloud.com>
2026-06-09 23:23:25 +0530

    k

--- out/html/data.js
+++ out/html/data.js
@@ -518,7 +518,7 @@ const tooltipList = [{
     },
     {
     searchString: "BSP-BLP",
-    explanationText: "<img src=img/logos/CPSL_Logo.png> Bolshevik Sama Samaja Party<br> & <br>Bolshevik-Leninist Party of India, Ceylon & Burma "
+    explanationText: "<img src=img/logos/CPSL_Logo.png> Bolshevik Sama Samaja Party<br> - <br>Bolshevik-Leninist Party of India, Ceylon & Burma "
     },
     {
     searchString: "PA",
